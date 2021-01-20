42e848311d084cbcc2482097fee6e6c52a5ae25b
Dylan Mitchell <dylan.mitchell2@g.austincc.edu>
2021-01-19 20:21:05 -0600

    new pages

---
+++ flashCards/subtractFlash.js
@@ -0,0 +1,59 @@
+const form = document.querySelector('input');
+const btn = document.querySelector('.submit')
+const card = document.querySelector('#card')
+const trumpets = document.getElementById('trumpets')
+const tryAgain = document.querySelector('label')
+
+
+
+btn.onclick = function () {
+    // make card flip
+    card.classList.toggle('flippy')
+    document.querySelector('label').innerText = "What Is The Answer?"
+    setTimeout(function () {
+        card.classList.remove('flippy');
+    }, 1900);
+    btn.classList.add('disabled')
+    // radom single digit generator
+    function addition(x, y) {
+        x = Math.floor(Math.random() * 9) + 1;
+        y = Math.floor(Math.random() * 9) + 1;
+        if (x < y) {
+            y = Math.floor(Math.random() * 1) + 1;
+        }
+        // add enter keypress listener
+        form.addEventListener('keypress', function (e) {
+            if (e.key === 'Enter') {
+
+                // make string into integer
+                // check for correct answer
+                // add text and color
+                if (parseInt(form.value) !== x - y) {
+                    document.querySelector('label').innerText = "Try Again"
+                    tryAgain.classList.add('text-danger')
+                } else {
+                    document.querySelector('label').innerText = "Correct!"
+                    tryAgain.classList.remove('text-danger')
+                    tryAgain.classList.add('text-success')
+                    btn.classList.remove('disabled')
+                    trumpets.play();
+                }
+            }
+        })
+
+        // make random digit generator switch with card flip
+        setTimeout(function () {
+            document.querySelector('.first').innerHTML = x;
+            // document.querySelector('.plus').innerHTML = "+";
+            document.querySelector('.second').innerHTML = "-" + y;
+        }, 500)
+
+        // make button appear if answer is correct
+        // remove color of text back to black after correct/incorrect answers
+        // remove text from submission box 
+        document.querySelector('.submit').innerHTML = "Next"
+        tryAgain.classList.remove('text-success')
+        form.value = ''
+    }
+    return addition();
+}
